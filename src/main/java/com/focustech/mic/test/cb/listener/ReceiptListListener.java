441fda8b362b01576fdae478ae8936127cc5f9e1
caiwen <nuist_kevin@163.com>
2017-04-20 23:33:35 +0800

    二次提交

--- src/main/java/com/focustech/mic/test/cb/listener/ReceiptListListener.java
+++ src/main/java/com/focustech/mic/test/cb/listener/ReceiptListListener.java
@@ -1,5 +1,7 @@
 package com.focustech.mic.test.cb.listener;
 
+import org.apache.activemq.command.ActiveMQTextMessage;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -16,6 +18,7 @@ public class ReceiptListListener implements MessageListener  {
                 System.out.println("离开家里卡我家二楼空间啊离开教室地方了空间撒到了房间啊老师看见对方快乐撒大家");
 
                 System.out.println(((TextMessage) message).getText());
+                System.out.println(((ActiveMQTextMessage) message).getGroupID());
             } catch (JMSException e) {
                 e.printStackTrace();
                 throw new RuntimeException(e);
