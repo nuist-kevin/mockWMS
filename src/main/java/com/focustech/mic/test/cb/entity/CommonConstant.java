5b3c63fbbc366c8d0a2e4f8cba4e67660dfd3337
caiwen <nuist_kevin@163.com>
2017-11-12 21:05:45 +0800

    维护公用常量

---
+++ src/main/java/com/focustech/mic/test/cb/entity/CommonConstant.java
@@ -0,0 +1,18 @@
+package com.focustech.mic.test.cb.entity;
+
+
+/**
+ * @author caiwen
+ */
+public class CommonConstant {
+
+  public static final String OPERATOR_NAME = "AUTO_MESSAGE";
+  public static final Integer OPERATOR_NO = 99;
+  public static final String WAREHOUSE_CODE = "FTUSCA001";
+
+  public static final String BILL_TYPE_CODE_ASN = "11";
+  public static final String BILL_TYPE_CODE_DO = "21";
+
+  public static final String OPERATION_TYPE_NEW = "new";
+  public static final String OPERATION_TYPE_CANCEL = "cancel";
+}
